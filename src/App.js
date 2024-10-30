991a0f4866d46534161fd604fee8b4520573fdbb
Eduardo <dudugamer929@gmail.com>
2024-10-29 22:26:15 -0400

    add Skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Banner } from './components/Banner';
 import { NavegationContainer } from './components/Navbar';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <NavegationContainer/>
       <Banner/>
+      <Skills/>
     </div>
   );
 }
